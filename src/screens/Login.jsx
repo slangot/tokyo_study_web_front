15d2546658bfd57299398bab0d5efbad219da7fe
Shawn <shawn17@live.fr>
2024-09-03 23:10:33 +0200

    🚧 add register link to login page

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -6,6 +6,9 @@ import{ useUser } from '../context/UserContext'
 // Icons
 import { FaEye, FaEyeSlash } from 'react-icons/fa6'
 
+// Packages
+import { Link } from 'react-router-dom';
+
 const Login = () => {
   const { state, dispatch } = useUser();
   const [email, setEmail] = useState("")
@@ -53,12 +56,15 @@ const Login = () => {
         )
         :
         (
-          <div className='flex flex-col mx-auto w-[80vw] bg-primary px-3 py-2 rounded-lg'>
+          <div className='flex flex-col mx-auto w-[80vw] md:w-[50vw] bg-primary px-3 py-5 rounded-lg'>
             <h1 className='mb-5'>Connectez-vous :</h1>
             <input type='text' className='w-full py-3 pl-2 mb-5 border-2 border-light-gray rounded-xl text-black' onChange={(e) => setEmail(e.target.value)} placeholder='Votre email' />
-            <div className='relative flex flex-row w-full h-10 items-center mb-7'>
+            <div className='relative flex flex-row w-full h-10 items-center mb-3'>
               <input type={showPassword ? 'text' :'password'} id='password' className='w-full py-3 mb-0 border-2 border-light-gray rounded-xl text-black pl-10' placeholder='Votre mot de passe' onChange={(e) => setPassword(e.target.value)} />
               <div onClick={() => setShowPassword(!showPassword)} className='absolute ml-3 text-black'>{showPassword ? <FaEyeSlash /> : <FaEye />}</div>
+            </div>
+            <div className='mt-0 mb-3'>
+              Pas encore de compte ? <Link to='/register' className='text-blue-500 underline font-bold'>Inscrivez-vous</Link>
             </div>
             <button className='bg-third py-2 px-2 w-auto mx-auto rounded-xl font-bold uppercase' onClick={() => connection()}>Se connecter</button>
           </div>
